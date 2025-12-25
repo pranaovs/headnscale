7836ebc988b3f3460fc01f4caac0e3c125262d26
Pranaov S <git@pranaovs.me>
2025-12-25 23:12:58 +0530

    refactor: make cut function better to return full string on no match
    tailscale source

--- internal/source/tailscale/extract.go
+++ internal/source/tailscale/extract.go
@@ -6,11 +6,6 @@ func hostNameFromDNSName(dnsName string) string {
 	// Remove trailing dot, if present
 	dnsName = strings.TrimSuffix(dnsName, ".")
 
-	// Split on the first dot and return the host
-	if before, _, ok := strings.Cut(dnsName, "."); ok {
-		return before
-	}
-
-	// Fallback: entire string is the host
-	return dnsName
+	before, _, _ := strings.Cut(dnsName, ".")
+	return before
 }
